626ecbae5af09ce66d93b15101eb5a5b305ada4d
David Hoyt <dhoyt@hoytsoft.org>
2011-07-29 19:05:17 -0700

    Added closest_fibonacci() method.

--- ruby/fibonacci/main/main.rb
+++ ruby/fibonacci/main/main.rb
@@ -1,4 +1,14 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+#!/usr/bin/env ruby
 
-puts "Hello World how are you?"
+require '../lib/fibonacci-gem.rb'
+
+def main
+	values    = [-100, -1, 0, 1, 144, 156, 99, 2000000]
+	#expected = [   0,  0, 0, 1, 144, 144, 89, 1346269]
+	
+	values.each do |value|
+		puts "#{value}.closest_fibonacci => #{value.closest_fibonacci}"
+	end
+end
+
+main
