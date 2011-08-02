047e6b4eae3e179d0059ab4d023731f2df958444
David Hoyt <dhoyt@hoytsoft.org>
2011-08-02 11:50:08 -0700

    Fixed indentation on fibonacci gem.

--- ruby/fibonacci/lib/fibonacci-gem.rb
+++ ruby/fibonacci/lib/fibonacci-gem.rb
@@ -1,17 +1,17 @@
 
 class Integer < Numeric
-	#This could be enhanced by precalculating a large table and then using a 
-	#binary search to find the closest value >= the number. If we reach the end 
-	#of the table and a value hasn't been found, then run the normal algorithm 
-	#starting with the last 2 numbers in the table.
+  #This could be enhanced by precalculating a large table and then using a 
+  #binary search to find the closest value >= the number. If we reach the end 
+  #of the table and a value hasn't been found, then run the normal algorithm 
+  #starting with the last 2 numbers in the table.
   def closest_fibonacci
-		i = self.to_i
-		a,b = 0,1
-		
-		until b > i
-			a,b = b,a+b
-		end
-		
-		a
-	end
+    i = self.to_i
+    a,b = 0,1
+    
+    until b > i
+      a,b = b,a+b
+    end
+    
+    a
+  end
 end
